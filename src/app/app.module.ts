1db481baac0207a99b6e1b5d403eee1b29264a52
okoeee <yk27.puxcm@gmail.com>
2023-06-29 08:31:52 +0900

    ヒーローエディタの作成まで

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent //ng generateで作成すると、declarationsに追加される
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule //ngModelを使うためには、←をimportする必要がある
   ],
   providers: [],
   bootstrap: [AppComponent]
